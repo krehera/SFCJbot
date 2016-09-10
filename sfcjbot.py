130974b3b4a3baa48b8aabb940c71e870d9da179
Aaron Kreher <chishx@gmail.com>
2016-09-10 21:52:12 +0000

    added DB connection

--- sfcjbot.py
+++ sfcjbot.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import MySQLdb
 
 client = discord.Client()
 
@@ -11,6 +12,15 @@ async def on_message(message):
 
 	if message.content.startswith('<@'+client.user.id+'>') or message.content.startswith('SFCJbot'):
 		await client.send_message(message.channel, 'received.')
+		command = message.content
+		if command.startswith('<@'+client.user.id+'>'):
+			idlength = len(client.user.id) + 3
+			command = command[idlength:]
+		else:
+			command = command[7:]
+		#remove whitespace from BEGINNING of command
+		command = command.lstrip()
+		
 		return
 
 	#remove this bit after all debugging is done
@@ -20,7 +30,14 @@ async def on_message(message):
 async def on_ready():
 	print('logged in as '+client.user.name)
 
+#This is probably the wrong way to do these things, but I don't care.
 f = open('.token', 'r')
 token = f.readline().strip('\n')
 f.close()
+f = open('.mysql_auth', 'r')
+db_user = f.readline().strip('\n')
+db_pwd = f.readline().strip('\n')
+db_host = f.readline().strip('\n')
+db_db = f.readline().strip('\n')
+db_connection = MySQLdb.connect(user=db_user, passwd = db_pwd, host=db_host, db=db_db)
 client.run(token)
